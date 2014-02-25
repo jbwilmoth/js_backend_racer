6ddbb31c31d0dfe0eb4faba7bec2bc97d0b25b68
Joe Wilmoth <jbwilmoth@gmail.com>
2014-02-24 21:49:23 -0600

    contoller methods structured

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,31 @@
+#===============GET=================
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+get '/game' do
+
+end
+
+get '/end_game' do
+
+end
+
+get '/stats/:url' do
+
+end
+
+#==============POST=================
+
+post '/game' do
+
+end
+
+post '/end_game'
+
+end
+
+
